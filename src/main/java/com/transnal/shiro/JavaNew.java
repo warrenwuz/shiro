ded727ca9aa534755d88480548928ee21dd986ca
wz <wuzhe1159@163.com>
2018-10-09 10:30:39 +0800

    日常练习

---
+++ src/main/java/com/transnal/shiro/JavaNew.java
@@ -0,0 +1,21 @@
+package com.transnal.shiro;
+
+
+
+import lombok.var;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Java11 测试
+ * @author wz
+ */
+public class JavaNew {
+    public static void main(String[] args) {
+        var java=10;
+        System.out.println(java);
+    }
+
+}
--- src/test/java/com/transnal/shiro/ShiroApplicationTests.java
+++ src/test/java/com/transnal/shiro/ShiroApplicationTests.java
@@ -20,7 +20,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import sun.security.provider.MD5;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
